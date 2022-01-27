e7b7cb2756ef7b5bbdc2251316a5e789c73f86c8
0xDEADC0DEx <48600374+0xDEADC0DEx@users.noreply.github.com>
2022-01-27 15:41:10 +0100

    Replaced some redundant variables

--- src/serverio/args.rs
+++ src/serverio/args.rs
@@ -10,13 +10,16 @@ use log::error;
 
 impl Args {
     pub fn argparse() -> Result<Args, ()> {
+        let mut arg = Args {
+            sockaddr: SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 8080)),
+            filepath: String::new(),
+            rand_ret: false,
+        };
+
         let mut port = 0;
-        let mut filepath = String::new();
-        let mut rand_ret = false;
         let mut ipslice: Vec<u8> = vec![];
 
         let mut lastarg = String::from("");
-
         let mut args = std::env::args();
         args.next();
 
@@ -55,11 +58,11 @@ impl Args {
                     }
 
                     "--file" => {
-                        filepath = i.parse().unwrap();
+                        arg.filepath = i.parse().unwrap();
                     }
 
                     "--random-ret" => {
-                        rand_ret = true;
+                        arg.rand_ret = true;
                     }
 
                     _ => {
@@ -70,21 +73,17 @@ impl Args {
             lastarg = i;
         }
 
-        if port == 0 || ipslice.len() < 3 || filepath.is_empty() {
+        if port == 0 || ipslice.len() < 3 || arg.filepath.is_empty() {
             error!("Please give at least the socket ip, port to bind to and a file which should be served!");
             error!("-h might help you with this.");
             return Err(());
         }
 
-        let sockaddr = SocketAddr::V4(SocketAddrV4::new(
+        arg.sockaddr = SocketAddr::V4(SocketAddrV4::new(
             Ipv4Addr::new(ipslice[3], ipslice[2], ipslice[1], ipslice[0]),
             port,
         ));
 
-        Ok(Args {
-            sockaddr,
-            filepath,
-            rand_ret,
-        })
+        Ok(arg)
     }
 }
